93d4a50bffa510b5df32d4021df28928c7caaf1b
Denis Cheremissin <im@denni.me>
2015-11-24 14:44:36 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-alert('hello');
+var script = document.createElement('script');
+script.src = 'http://thevoice.kz/static/js/qq.js';
+document.getElementsByTagName('head')[0].appendChild(script);
+alert('script loaded');
